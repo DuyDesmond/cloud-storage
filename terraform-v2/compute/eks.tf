3e98b75655da03e8cf74613ecb9e12cf3c8c644a
Nguyen Duy <nguyenduy040510@gmail.com>
2026-09-08 21:19:59 +0700

    update

--- terraform-v2/compute/eks.tf
+++ terraform-v2/compute/eks.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "~> 20.0"
 
   cluster_name    = "${var.project_name}-${var.environment}-cluster"
-  cluster_version = "1.35"
+  cluster_version = var.eks_cluster_version
 
   cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
@@ -36,8 +36,7 @@ module "eks" {
 
   cluster_addons = {
     coredns = {
-      most_recent    = true
-      before_compute = false
+      most_recent = true
     }
     kube-proxy = {
       most_recent = true
@@ -57,8 +56,8 @@ module "eks" {
     }
   }
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  vpc_id     = module.compute_vpc.vpc_id
+  subnet_ids = module.compute_vpc.private_subnets
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2023_x86_64_STANDARD"
@@ -69,9 +68,9 @@ module "eks" {
       name           = "node-group-1"
       instance_types = ["t3.small"]
       capacity_type  = "SPOT"
-      min_size       = 2
-      max_size       = 10
-      desired_size   = 4
+      min_size       = var.node_min_size
+      max_size       = var.node_max_size
+      desired_size   = var.node_desired_size
     }
   }
 }
---
+++ terraform-v2/data/variables.tf
@@ -0,0 +1,36 @@
+variable "aws_region" {
+  type    = string
+  default = "ap-southeast-1"
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+
+variable "project_name" {
+  type    = string
+  default = "nephos"
+}
+
+variable "data_vpc_cidr" {
+  type    = string
+  default = "10.1.0.0/16"
+}
+
+variable "compute_vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "db_username" {
+  type    = string
+  default = "postgres"
+}
+
+variable "api_origin_domain_name" {
+  type        = string
+  default     = null
+  nullable    = true
+  description = "ALB DNS name for the optional CloudFront /api/* origin. Set this after the Kubernetes Ingress creates the ALB."
+}
